921a2309e2d83e0262640445a57e2bdc956a885c
Dong Nguyen <ndaidong@gmail.com>
2016-07-08 16:51:54 +0700

    Update layout with parallax effect

--- src/js/app.js
+++ src/js/app.js
@@ -6,18 +6,18 @@
 
 (() => {
 
-  'use strict';
-
   var doc = window.doc;
   var tw = window.TypeWritter;
 
   doc.ready(() => {
-    setTimeout(() => {
-      tw.start({
-        containerId: 'typingArea',
-        extractClass: 'sentence',
-        cursorClass: 'cursor'
-      });
-    }, 1000);
+    if (doc.get('typingArea')) {
+      setTimeout(() => {
+        tw.start({
+          containerId: 'typingArea',
+          extractClass: 'sentence',
+          cursorClass: 'cursor'
+        });
+      }, 3000);
+    }
   });
 })();
